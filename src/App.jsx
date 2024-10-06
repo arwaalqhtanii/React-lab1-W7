a1c5fadcd2219337d326d7db27dc40d78343bcab
arwaalqhtanii <alqhtaniarwa414@gmail.com>
2024-10-06 11:37:22 +0300

    frist

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './assets/pages/HomePage';
 import AddCharacterPage from './assets/pages/AddCharacterPage';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/add",
+    element: <AddCharacterPage />,
+  }
+]);
+
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/add" element={<AddCharacterPage />} />
-      </Routes>
-    </Router>
+    <RouterProvider router={router} />
   );
 };
 
 export default App;
+
